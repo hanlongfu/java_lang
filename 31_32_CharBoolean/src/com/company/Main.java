022f0e23a6096bc248b0e1d5a0868979910adbd9
Hanlong Fu <hfu@salemstate.edu>
2021-09-16 22:50:57 -0700

    String

--- 31_32_CharBoolean/src/com/company/Main.java
+++ 31_32_CharBoolean/src/com/company/Main.java
@@ -2,7 +2,35 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    // char occupies 16 bits (2 bytes)
+    char myChar = 'D';
+    // see unicode-table.com
+    char myUnicodeD = '\u0044';
+    char myUnicodeCopy = '\u00A9';
+    System.out.println(myUnicodeD);
+    System.out.println(myUnicodeCopy);
+
+    //Boolean Type
+    boolean myTrueBoolean = true;
+    boolean myFalseBoolean = false;
+    System.out.println(myTrueBoolean);
+    System.out.println(false);
+
+    //String is a class not a primitive
+    String myString = "This is a string";
+    System.out.println("myString is equal to: " + myString);
+
+    //append a String
+    myString += ", and this is more.";
+    System.out.println("myString is equal to: " + myString);
+
+    //implicit conversion (convert to lower-level data types)
+    int myInt = 50;
+    String lastString = "10";
+    lastString = lastString + myInt;
+    System.out.println(lastString);
+
+    //Strings are immutable
+  }
 }
